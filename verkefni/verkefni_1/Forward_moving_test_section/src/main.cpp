f575cdf5ecbaa33eb619ab81f4340691c3585afe
Asmodeus Cruentus <asmodeus.cruentus@gmail.com>
2025-08-28 10:19:13 +0000

    re-uploaded V1, cause github sucks

---
+++ verkefni/verkefni_1/Forward_moving_test_section/src/main.cpp
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Wed Sep 25 2019                                           */
+/*    Description:  Moving Forward (mm)                                       */
+/*                                                                            */
+/*    This Program drives the robot forward for 150 millimeters.              */
+/*                                                                            */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    1(left wheel), 3(right wheel), D
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  int i = 500;
+
+while (true) {
+  if (i >= 2500) {break;}
+
+  Drivetrain.driveFor(forward, i, mm);
+  wait(200, msec);
+
+  Drivetrain.driveFor(reverse, i, mm);
+  wait(200, msec); 
+
+  i += 500;  
+}
+
+
+}
---
+++ verkefni/verkefni_1/readme.md
@@ -0,0 +1,32 @@
+**Verkefni 1**
+
+
+    Port 1 = Left Front Wheel
+    port 2 = ??
+    port 3 = Right Front Wheel
+    port 4 = Claw open/close
+    port 5 = something in the back
+    port 6 = ??
+    port 7 = ??
+    port 8 = ??
+    port 9 = ??
+    port 10 = ??
+    port 20 = inertia sensor
+
+
+![Alt text](./robotics_thinkmap_part_1.drawio.png)
+
+
+
+**Video demonstration of part 1**
+<br>
+![alt text](VID_1.gif)
+
+
+![Alt text](./Robotics_thinkmap_part_2.drawio.png)
+
+
+
+**Video Demonstration of part 2**
+
+![alt text](VID_2.2.gif)
