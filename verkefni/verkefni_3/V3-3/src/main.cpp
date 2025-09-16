818c0a48da940a04f312be38705b156588fd7e58
gunnar112 <145785074+gunnar112@users.noreply.github.com>
2025-09-16 14:09:40 +0000

    Update main.cpp

--- verkefni/verkefni_3/V3-3/src/main.cpp
+++ verkefni/verkefni_3/V3-3/src/main.cpp
@@ -3,19 +3,18 @@
 
 using namespace vex;
 
-// Skilgreiningar
+
 const int CENTER_FOV = 158;  
-// const int OFFSET_X = 25;     
+const int DEAD_X = 40;       
 
 std::string state = "Stoppar"; 
 
-// Thread sem sér um skjáinn
 int displayTask() {
   while (true) {
     Brain.Screen.clearScreen();
     Brain.Screen.setCursor(1, 1);
     Brain.Screen.print("Staða: %s", state.c_str());
-    this_thread::sleep_for(200); 
+    this_thread::sleep_for(400); 
   }
   return 0;
 }
@@ -27,37 +26,39 @@ int main() {
 
   while (true) {
     Vision5.takeSnapshot(Vision5__GREENBOX);
-    Brain.Screen.clearScreen();
-    Brain.Screen.setCursor(1, 1);
-    Brain.Screen.print(Vision5.largestObject.centerX);
+    
+
     if (Vision5.largestObject.exists) {
-      int objX = Vision5.largestObject.centerX;
+      int objX     = Vision5.largestObject.centerX;
       int objWidth = Vision5.largestObject.width;
-      
-      if(objWidth > 150){
+
+      if (objX > CENTER_FOV + DEAD_X) {
+        RightMotor.spin(reverse,10,percent);
+        LeftMotor.spin(forward,10,percent);
+        task::sleep(10);
+        state = "Beygir til hægri";
+      } 
+      else if (objX < CENTER_FOV - DEAD_X) {
+        RightMotor.spin(forward,10,percent);
+        LeftMotor.spin(reverse,10,percent);
+        task::sleep(10);
+        state = "Beygir til vinstri";
+      }
+      else if (objWidth > 150) {
         LeftMotor.spin(reverse);
-        RightMotor.spin(reverse);}
-      
+        RightMotor.spin(reverse);
+        state = "Bakkar (hlutur of nálægt)";
+      } 
       else if (objWidth < 100) {
         LeftMotor.spin(forward);
         RightMotor.spin(forward);
-        state = "Bakkar (hlutur of nálægt)";
+        state = "Fer áfram (hlutur of langt)";
       } 
-     
-      
-      else if(Vision5.largestObject.centerX > CENTER_FOV + 30 && objWidth < 100){
-        RightMotor.spin(reverse);
-        LeftMotor.spin(forward);
-        task::sleep(10);
-      }
-      else if (Vision5.largestObject.centerX < CENTER_FOV - 30 && objWidth < 100) {
-        RightMotor.spin(forward);
-        LeftMotor.spin(reverse);
-      }
-      
-      else{
+      else {
         LeftMotor.stop(brakeType::brake);
-        RightMotor.stop(brakeType::brake);}
+        RightMotor.stop(brakeType::brake);
+        state = "Miðjað og í lagi";
+      }
     }
     else {
       LeftMotor.stop(brakeType::brake);
