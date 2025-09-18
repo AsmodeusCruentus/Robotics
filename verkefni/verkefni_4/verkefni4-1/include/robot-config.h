83ac303d9416e4b785913f51b58b2d415da345e1
gunnar112 <145785074+gunnar112@users.noreply.github.com>
2025-09-18 09:38:31 +0000

    Add files via upload

---
+++ verkefni/verkefni_4/verkefni4-1/include/robot-config.h
@@ -0,0 +1,12 @@
+using namespace vex;
+
+extern brain Brain;
+
+extern motor LeftMotor;
+extern motor RightMotor;
+extern line LineLeft;
+extern line LineCenter;
+extern line LineRight;
+extern distance DistSensor;
+
+void vexcodeInit(void);
